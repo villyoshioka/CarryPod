84e1dcaa90f0fe7e5bb4dbb2ef18fe4c4407e50e
vill_yoshioka <vill.yoshioka@icloud.com>
2026-09-17 11:07:24 +0900

    v3.2.2

--- lib/action-scheduler/vendor/composer/installed.php
+++ lib/action-scheduler/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'woocommerce/action-scheduler',
-        'pretty_version' => 'dev-release/4.1.0',
-        'version' => 'dev-release/4.1.0',
-        'reference' => '40a3df93a251590c58717b91b1049a13410e2ac4',
+        'pretty_version' => 'dev-release/4.2.0',
+        'version' => 'dev-release/4.2.0',
+        'reference' => '9e2c6b02e89652bade4445ea2ca5fb12e0fa7242',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'woocommerce/action-scheduler' => array(
-            'pretty_version' => 'dev-release/4.1.0',
-            'version' => 'dev-release/4.1.0',
-            'reference' => '40a3df93a251590c58717b91b1049a13410e2ac4',
+            'pretty_version' => 'dev-release/4.2.0',
+            'version' => 'dev-release/4.2.0',
+            'reference' => '9e2c6b02e89652bade4445ea2ca5fb12e0fa7242',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
